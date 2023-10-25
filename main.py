dab49380bdfb0195211bf498119d59e7c57ed461
KAN-JUNHO <junho6667@gmail.com>
2023-10-25 10:10:13 +0900

    test 2023-10-25

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import logging
-
-from fastapi import FastAPI, Request, Form, Query
-from fastapi.templating import Jinja2Templates
 from datetime import datetime
-from dotenv import load_dotenv
 
 import openai
-import os
-
+from decouple import config
+from fastapi import FastAPI, Form
+from fastapi.templating import Jinja2Templates
+from langchain.chains import LLMChain
+from langchain.llms.openai import OpenAI
+from langchain.prompts import PromptTemplate
 from pydantic import BaseModel
 
-app = FastAPI()
-
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
+app = FastAPI(debug=True)
 
 
 templates = Jinja2Templates(directory="templates")
@@ -76,53 +72,79 @@ async def chat_response(
     print(f"Duration (days): {duration}")
 
     user_text = (f" my infomation is Height: {height} cm, Age: {age} years, "
-                 f"Current Weight: {current_weight} kg, Target Weight: {target_weight} kg, Duration: {duration} days, Please recommend Diet food receipt and translate in Korean")
+                 f"Current Weight: {current_weight} kg, Target Weight: {target_weight} kg, Duration: {duration} days, Please recommend Diet food receipt")
+
+    good_qa_prompt = PromptTemplate(
+        template=""""If my text does not contain any food, drink, or fruit, respond with 'No'. If it does, respond with 'Yes'
+
+        Question: {question}
+
+        """,
+        input_variables=["question"],
+    )
+
+    llm = OpenAI(temperature=0)
+
+    good_qa_chain = LLMChain(llm=llm, prompt=good_qa_prompt)
+
+    judgment = good_qa_chain.run(question=user_message)
+
+    if judgment == '\nYes':
+
+        user_message += user_text
+
+        messages.append({"role": "user", "content": user_message})
+        print(messages)
+
+        try:
+            # 여기에서 사용자의 입력 정보(height, weight, target_weight)를 사용하여 BMI 계산 등을 수행할 수 있습니다.
+            # BMI 계산 코드를 여기에 추가하십시오.
+            response_text = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=user_message,
+                max_tokens=2000
+            )
+
+            text = response_text.choices[0].text
+
+            response_image = openai.Image.create(
+                prompt=text,
+                n=1,
+                size="1024x1024"
+            )
 
 
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    user_message += user_text
+            image_url = response_image['data'][0]['url']
 
-    messages.append({"role": "user", "content": user_message})
-    print(messages)
+            messages.append(
+                {"role": "assistant", "content": "다음은 요청하신 식단입니다:", "text":text, "image_url": image_url, "current_time": current_time})
+        except Exception as e:
+            if 'safety system' in str(e):
+                messages.append({"role": "assistant", "content": f"safety system: {e}"})
+            else:
+                messages.append({"role": "assistant", "content": f"오류: {e}"})
 
-    try:
-        # 여기에서 사용자의 입력 정보(height, weight, target_weight)를 사용하여 BMI 계산 등을 수행할 수 있습니다.
-        # BMI 계산 코드를 여기에 추가하십시오.
-        response_text = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_message,
-            max_tokens=2000
-        )
-
-        text = response_text.choices[0].text
-
-        response_image = openai.Image.create(
-            prompt=text,
-            n=1,
-            size="1024x1024"
-        )
-
-
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-        image_url = response_image['data'][0]['url']
-
-        messages.append(
-            {"role": "assistant", "content": "다음은 요청하신 식단입니다:", "text":text, "image_url": image_url, "current_time": current_time})
-    except Exception as e:
-        if 'safety system' in str(e):
-            messages.append({"role": "assistant", "content": f"safety system: {e}"})
-        else:
-            messages.append({"role": "assistant", "content": f"오류: {e}"})
-
-    return templates.TemplateResponse("index.html", {
-        "request":{},
-        "height": height,
-        "age": age,
-        "current_weight": current_weight,
-        "target_weight": target_weight,
-        "duration": duration,
-        "messages": messages
-    })
-
+        return templates.TemplateResponse("index.html", {
+            "request":{},
+            "height": height,
+            "age": age,
+            "current_weight": current_weight,
+            "target_weight": target_weight,
+            "duration": duration,
+            "messages": messages
+        })
+    else:
+        # 음식, 음료, 또는 과일과 관련이 없는 경우의 메시지 처리
+        messages.append({"role": "assistant", "content": "제대로 된 값을 입력해주세요."})
+        return templates.TemplateResponse("index.html", {
+            "request":{},
+            "height": height,
+            "age": age,
+            "current_weight": current_weight,
+            "target_weight": target_weight,
+            "duration": duration,
+            "messages": messages
+        })
 # 추가 폼 요소(height, weight, target_weight)를 템플릿에 추가해야 합니다.
